e9bac8370b0191bd0ceceaec841d1f80c2c2dfc0
Lukas Lötscher <lukas@loetscher.swiss>
2025-04-02 19:54:03 +0200

    added base application

---
+++ src/main/java/mytryoutgroupid/programmingmy/training/Application.java
@@ -0,0 +1,13 @@
+package mytryoutgroupid.programmingmy.training
+
+public class APplication 
+	public: Application(){
+			System.out.println("Inside Application");
+
+		}
+
+	public static void main(String[] args){
+		System.out.println("Starting Application);
+		Application app = new Application();
+	}
+}
