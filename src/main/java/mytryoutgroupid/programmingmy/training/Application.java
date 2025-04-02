7e09cf9364ba053879ed28b11c9ca863b5cf1c62
Lukas Lötscher <lukas@loetscher.swiss>
2025-04-02 20:31:05 +0200

    fixed mistakes in pom file (stackoverflow still ebtter then chatgtp:) ; also fixe java file -> coding without an ide is hard. i willswitch sson back to eclipse..., but first i want to understand how this works so i do not miss stuff due to the ide.

--- src/main/java/mytryoutgroupid/programmingmy/training/Application.java
+++ src/main/java/mytryoutgroupid/programmingmy/training/Application.java
@@ -1,13 +1,13 @@
-package mytryoutgroupid.programmingmy.training
+package mytryoutgroupid.programmingmy.training;
 
-public class APplication 
-	public: Application(){
+public class Application{ 
+	public Application(){
 			System.out.println("Inside Application");
 
-		}
+		};
 
 	public static void main(String[] args){
-		System.out.println("Starting Application);
+		System.out.println("Starting Application");
 		Application app = new Application();
-	}
-}
+	};
+};
