e1b7c71b744b91f18a61afb89d83545c85f18dc9
Lukas Loetscher <Lukas@Loetscher.swiss>
2025-04-06 20:58:20 +0200

    added external libraries.

--- src/main/java/mytryoutgroupid/programmingmy/training/Application.java
+++ src/main/java/mytryoutgroupid/programmingmy/training/Application.java
@@ -1,6 +1,23 @@
 package mytryoutgroupid.programmingmy.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Application{ 
+
+	public void greet() {
+		List<String>greetings = new ArrayList<>();
+		greetings.add("Hello");
+		greetings.add("Hallo");
+		greetings.add("Bonjour");
+		for(String greeting : greetings){
+			System.out.println("Greeting; "+greeting);
+		}
+	
+	};
+
 	public Application(){
 			System.out.println("Inside Application");
 
